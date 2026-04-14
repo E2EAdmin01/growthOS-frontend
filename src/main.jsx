f091fc448c5dcd76da76f6f30d88d329bec7905b
Dan Pritchard <dan.pritchard@e2e-integration.co.uk>
2026-04-14 11:59:12 +0100

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GrowthOS from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GrowthOS />
+  </React.StrictMode>,
+)
